3f39e970a5d31dedfe1f30428749d4908c9ab3f8
Mathias <mathias.minacapilli@mercadolibre.com>
2020-08-28 11:37:37 -0300

    Add request of pomodoro objective and display while displaying the remaining time

--- main.py
+++ main.py
@@ -17,18 +17,20 @@ def get_remaining_seconds_to_next_minute (total_seconds):
     seconds = total_seconds - (minutes * SECONDS_IN_ONE_MINUTE)
     return seconds
 
-def countdown(t):
+def countdown(t, objective):
     while t > 0:
         os.system("clear") # TODO: fix this not working for windows
         parsed_remaining_time = parse_seconds_to_minutes(t)
         parsed_remaining_time_seconds = get_remaining_seconds_to_next_minute(t)
+        print(f"Pomodoro objective/s: {objective}")
         print(f"Remaining time: {parsed_remaining_time}:{parsed_remaining_time_seconds}")
         t -= 1
         time.sleep(1)
     print("BLAST OFF!")
 
 if __name__ == "__main__":
-    countdown(POMODORO_SECONDS)
+    objective = input("Please, enter your objective for this pomodoro: ")
+    countdown(POMODORO_SECONDS, objective)
     i = 0
     for i in [1,2,3]:
         send_sound_alert()
